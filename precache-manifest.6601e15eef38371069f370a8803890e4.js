fbbdfe5d70d81592ef0aa0edc1b68ce4add42b9e
narasimhan26 <narasimhanaakash26@gmail.com>
2019-10-19 21:25:07 +0530

    Updates

---
+++ precache-manifest.6601e15eef38371069f370a8803890e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ea98952ed81eb4fd02eea905d56d4d",
+    "url": "/blank-bank/index.html"
+  },
+  {
+    "revision": "625c064b7f933cd02b5b",
+    "url": "/blank-bank/static/js/2.10043b9b.chunk.js"
+  },
+  {
+    "revision": "8710daa2dcf0d4a51741",
+    "url": "/blank-bank/static/js/main.220dda34.chunk.js"
+  },
+  {
+    "revision": "0092c13e8cc41d7d008a",
+    "url": "/blank-bank/static/js/runtime-main.edab9795.js"
+  }
+]);
